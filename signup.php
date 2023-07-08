db09e032700ab3196d961f227c8a6e8fbcfa8bd6
GAUDIA <202110228@fit.edu.ph>
2023-07-08 12:16:50 +0800

    Added Age

--- signup.php
+++ signup.php
@@ -156,7 +156,7 @@
 <main class="form-signin w-100 m-auto">
   <form>
     <img class="mb-4" src="./assets/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
@@ -166,15 +166,17 @@
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
-
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Age">
+      <label for="floatingInput">Age</label>
     </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
+    <div class="text-center">
+        Already have an account? <a href="signin.php">Sign in</a>
+    </div>
     <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+    
   </form>
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
