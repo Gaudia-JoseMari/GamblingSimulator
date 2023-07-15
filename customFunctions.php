c7dc320be14b1e3b0462e43e4453268d70879576
Gaudia-JoseMari <202110228@fit.edu.ph>
2023-07-15 23:01:48 +0800

    Add inventory in navbar

--- customFunctions.php
+++ customFunctions.php
@@ -3,10 +3,11 @@
         ?>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Gambling</a>
+            <a class="navbar-brand" href="home.php">Gambling</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <a class="nav-link" href="inventory.php">Inventory</a>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                 <li class="nav-item dropdown">
