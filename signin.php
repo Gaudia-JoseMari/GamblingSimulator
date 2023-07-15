c518716a8bdf7e6e8137bd064f2b19ca9c4a160e
KnightRez <rezcors@gmail.com>
2023-07-15 23:08:17 +0800

    move css styling inside respective pages

--- signin.php
+++ signin.php
@@ -104,11 +104,35 @@
       .error{
         color: red;
       }
+      html,
+      body {
+        height: 100%;
+      }
+
+      .form-signin {
+        max-width: 330px;
+        padding: 1rem;
+      }
+
+      .form-signin .form-floating:focus-within {
+        z-index: 2;
+      }
+
+      .form-signin input[type="text"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-right-radius: 0;
+        border-top-left-radius: 0;
+      }
     </style>
 
     
     <!-- Custom styles for this template -->
-    <link href="./css/signin.css" rel="stylesheet">
   </head>
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
--- signup.php
+++ signup.php
@@ -104,11 +104,52 @@
       .error {
         color: red;
       }
+
+      html,
+      body {
+        height: 100%;
+      }
+
+      .form-signin {
+        max-width: 330px;
+        padding: 1rem;
+      }
+
+      .form-signin .form-floating:focus-within {
+        z-index: 2;
+      }
+
+      .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[placeholder="Username"] {
+        margin-bottom: -1px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[type="password"] {
+        margin-bottom: -1px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[placeholder="Age"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
     </style>
 
     
     <!-- Custom styles for this template -->
-    <link href="./css/sign-up.css" rel="stylesheet">
   </head>
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
