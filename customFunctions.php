ca20e7d84b327ea8300c7d1de5fd924ebe1b785e
KnightRez <rezcors@gmail.com>
2023-07-15 18:56:50 +0800

    add customFunctions php and navbar function

---
+++ customFunctions.php
@@ -0,0 +1,31 @@
+<?php
+    function navbar() {
+        ?>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">Gambling</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Games
+                    </a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Case Opening</a></li>
+                    </ul>
+                </li>
+                </ul>
+                <ul class="navbar-nav ms-auto"> 
+                <li class="nav-item">
+                    <a class="btn btn-dark" href="signout.php" role="button">Sign out</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        <?php
+    }
+?>
---
+++ play_solo.php
@@ -0,0 +1,24 @@
+<?php include "customFunctions.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        navbar();
+    ?>
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-l-4 border p3">Hello World</div>
+            <div class="col-sm-4 border p3">Hello World</div>
+            <div class="col-sm-4 border p3">Hello World</div>
+        </div>
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
