43681c77404e0c7ccb7d326888b96f1c710aea34
Gaudia-JoseMari <202110228@fit.edu.ph>
2023-07-16 23:00:36 +0800

    Add window icon

--- inventory.php
+++ inventory.php
@@ -36,6 +36,7 @@ if (isset($_POST['sell'])) {
     <title>Gambling</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="css/home.css">
+    
 </head>
 <style>
     .Industrial {
