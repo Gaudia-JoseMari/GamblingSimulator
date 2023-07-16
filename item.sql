b5c1dfe671a2b2822baa0c3c8646e6c7e075c0c0
KnightRez <rezcors@gmail.com>
2023-07-16 21:49:27 +0800

    update tables

--- item.sql
+++ item.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2023 at 04:25 AM
+-- Generation Time: Jul 16, 2023 at 03:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,6 +35,50 @@ CREATE TABLE `item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `user_id`, `masteritem_id`, `item_name`) VALUES
+(2, 0, 13, 'MP5-SD Agent'),
+(3, 0, 16, 'P250 Inferno'),
+(4, 0, 21, 'CZ75-Auto Circaetus'),
+(5, 0, 13, 'MP5-SD Agent'),
+(6, 0, 7, 'FAMAS Decommissioned'),
+(7, 0, 12, 'MAG-7 Popdog'),
+(8, 0, 6, 'FAMAS Commemoration'),
+(9, 0, 7, 'FAMAS Decommissioned'),
+(10, 0, 10, 'M249 Aztec'),
+(11, 0, 11, 'MAC-10 Classic Crate'),
+(12, 0, 30, 'Nova Windblown'),
+(13, 0, 24, 'Glock-18 Clear Polymer'),
+(14, 0, 22, 'Desert Eagle Trigger Discipline'),
+(15, 0, 33, 'SG 553 Heavy Metal'),
+(16, 0, 11, 'MAC-10 Classic Crate'),
+(17, 0, 17, 'SCAR-20 Assault'),
+(18, 1, 25, 'M4A4 In Living Color'),
+(19, 1, 9, 'Glock-18 Sacrifice'),
+(20, 1, 13, 'MP5-SD Agent'),
+(21, 1, 27, 'MAC-10 Button Masher'),
+(22, 1, 26, 'M249 O.S.I.P.R.'),
+(23, 1, 36, 'XM1014 XOXO'),
+(24, 1, 23, 'Galil AR Chromatic Aberration'),
+(25, 1, 31, 'P250 Cyber Shell'),
+(26, 1, 32, 'R8 Revolver Junk Yard'),
+(27, 1, 35, 'USP-S The Traitor'),
+(28, 1, 32, 'R8 Revolver Junk Yard'),
+(29, 1, 36, 'XM1014 XOXO'),
+(30, 1, 16, 'P250 Inferno'),
+(31, 1, 3, 'Dual Berettas Elite 1.6'),
+(32, 1, 35, 'USP-S The Traitor'),
+(33, 1, 24, 'Glock-18 Clear Polymer'),
+(34, 1, 34, 'UMP-45 Oscillator'),
+(35, 1, 21, 'CZ75-Auto Circaetus'),
+(36, 1, 20, 'AK-47 Slate'),
+(37, 1, 25, 'M4A4 In Living Color'),
+(38, 1, 32, 'R8 Revolver Junk Yard'),
+(39, 1, 22, 'Desert Eagle Trigger Discipline');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -54,7 +98,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Constraints for dumped tables
--- masteritem.sql
+++ masteritem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2023 at 12:04 PM
+-- Generation Time: Jul 16, 2023 at 03:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -51,7 +51,7 @@ INSERT INTO `masteritem` (`masteritem_id`, `item_name`, `item_type`, `collection
 (6, 'FAMAS_Commemoration', 'FAMAS', 'CS20', 15.23, 'Covert', 'CSGO', 'FAMAS_Commemoration.png'),
 (7, 'FAMAS_Decommissioned', 'FAMAS', 'CS20', 0.36, 'Mil-spec', 'CSGO', 'FAMAS_Decommissioned.png'),
 (8, 'Five-SeveN_Buddy', 'Five_seveN', 'CS20', 1.23, 'Restricted', 'CSGO', 'Five-SeveN_Buddy.png'),
-(9, 'Glock-18_Sacrifice', 'Glock-18', 'CS20', 4.92, 'Mil_Spec', 'CSGO', 'Glock-18_Sacrifice.png'),
+(9, 'Glock-18_Sacrifice', 'Glock-18', 'CS20', 4.92, 'Mil-spec', 'CSGO', 'Glock-18_Sacrifice.png'),
 (10, 'M249_Aztec', 'M249', 'CS20', 0.42, 'Restricted', 'CSGO', 'M249_Aztec.png'),
 (11, 'MAC-10_Classic_Crate', 'MAC-10', 'CS20', 0.2, 'Mil-Spec', 'CSGO', 'MAC-10_Classic_Crate.png'),
 (12, 'MAG-7_Popdog', 'MAG-7', 'CS20', 0.97, 'Mil-spec', 'CSGO', 'MAG-7_Popdog.png'),
@@ -61,7 +61,24 @@ INSERT INTO `masteritem` (`masteritem_id`, `item_name`, `item_type`, `collection
 (16, 'P250_Inferno', 'P250', 'CS20', 2.29, 'Restricted', 'CSGO', 'P250_Inferno.png'),
 (17, 'SCAR-20_Assault', 'SCAR-20', 'CS20', 0.23, 'Mil-Spec', 'CSGO', 'SCAR-20_Assault.png'),
 (18, 'Tec-9_Flash_Out', 'Tec-9', 'CS20', 0.1, 'Industrial', 'CSGO', 'Tec-9_Flash_Out.png'),
-(19, 'UMP-45_Plastique', 'UMP-45', 'CS20', 0.53, 'Restricted', 'CSGO', 'UMP-45_Plastique.png');
+(19, 'UMP-45_Plastique', 'UMP-45', 'CS20', 0.53, 'Restricted', 'CSGO', 'UMP-45_Plastique.png'),
+(20, 'AK-47_Slate', 'AK-47', 'Snakebite', 38.09, 'Restricted', 'CSGO', 'AK-47_Slate.png'),
+(21, 'CZ75-Auto_Circaetus', 'CZ75-Auto', 'Snakebite', 1.55, 'Mil-spec', 'CSGO', 'CZ75-Auto_Circaetus.png'),
+(22, 'Desert_Eagle_Trigger_Discipline', 'Desert_Eagle', 'Snakebite', 3.71, 'Restricted', 'CSGO', 'Desert_Eagle_Trigger_Discipline.png'),
+(23, 'Galil_AR_Chromatic_Aberration', 'Galil_AR', 'Snakebite', 25.93, 'Classified', 'CSGO', 'Galil_AR_Chromatic_Aberration.png'),
+(24, 'Glock-18_Clear_Polymer', 'Glock-18', 'Snakebite', 2.78, 'Industrial', 'CSGO', 'Glock-18_Clear_Polymer.png'),
+(25, 'M4A4_In_Living_Color', 'M4A4', 'Snakebite', 11.84, 'Covert', 'CSGO', 'M4A4_In_Living_Color.png'),
+(26, 'M249_O.S.I.P.R.', 'M249', 'Snakebite', 1.17, 'Industrial', 'CSGO', 'M249_O.S.I.P.R..png'),
+(27, 'MAC-10_Button_Masher', 'MAC-10', 'Snakebite', 1.11, 'Restricted', 'CSGO', 'MAC-10_Button_Masher.png'),
+(28, 'MP9_Food_Chain', 'MP9', 'Snakebite', 3.48, 'Classified', 'CSGO', 'MP9_Food_Chain.png'),
+(29, 'Negev_dev_texture', 'Negev', 'Snakebite', 2.87, 'Restricted', 'CSGO', 'Negev_dev_texture.png'),
+(30, 'Nova_Windblown', 'Nova', 'Snakebite', 0.25, 'Mil-spec', 'CSGO', 'Nova_Windblown.png'),
+(31, 'P250_Cyber_Shell', 'P250', 'Snakebite', 0.58, 'Restricted', 'CSGO', 'P250_Cyber_Shell.png'),
+(32, 'R8_Revolver_Junk_Yard', 'R8_Revolver', 'Snakebite', 0.25, 'Mil-spec', 'CSGO', 'R8_Revolver_Junk_Yard.png'),
+(33, 'SG_553_Heavy_Metal', 'SG_553', 'Snakebite', 0.23, 'Industrial', 'CSGO', 'SG_553_Heavy_Metal.png'),
+(34, 'UMP-45_Oscillator', 'UMP-45', 'Snakebite', 0.2, 'Mil-spec', 'CSGO', 'UMP-45_Oscillator.png'),
+(35, 'USP-S_The_Traitor', 'USP-S', 'Snakebite', 24.65, 'Covert', 'CSGO', 'USP-S_The_Traitor.png'),
+(36, 'XM1014_XOXO', 'XM1014', 'Snakebite', 22.82, 'Classified', 'CSGO', 'XM1014_XOXO.png');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +98,7 @@ ALTER TABLE `masteritem`
 -- AUTO_INCREMENT for table `masteritem`
 --
 ALTER TABLE `masteritem`
-  MODIFY `masteritem_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `masteritem_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
